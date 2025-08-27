5fe273aa7c0a1bf834e41a6057dcabefbadf5996
Klevis Imeri <klevisimeri11@gmail.com>
2025-08-27 15:57:09 +0200

    Formated the display.sh to easy add more monitors and added more monitors

--- display.sh
+++ display.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# Define monitor names
-EXTERNAL_MONITOR_1="HDMI-1-1"
-EXTERNAL_MONITOR_2="HDMI-1-4"
+EXTERNAL_MONITORS=("HDMI-1-1" "HDMI-1-2" "HDMI-1-4" "HDMI-1" "HDMI-2")
 INTERNAL_MONITOR="eDP-1"
 
-# Get the connection status of the external monitors
-STATUS_MONITOR_1=$(xrandr | grep "$EXTERNAL_MONITOR_1" | awk '{print $2}')
-STATUS_MONITOR_2=$(xrandr | grep "$EXTERNAL_MONITOR_2" | awk '{print $2}')
+EXTERNAL_MONITOR_FOUND=false
 
-# Determine which monitor is connected and set it as primary
-if [[ "$STATUS_MONITOR_1" == "connected" ]]; then
-    echo "External monitor $EXTERNAL_MONITOR_1 detected. Setting it as primary."
-    xrandr --output "$EXTERNAL_MONITOR_1" --primary --auto --output "$INTERNAL_MONITOR" --off
-elif [[ "$STATUS_MONITOR_2" == "connected" ]]; then
-    echo "External monitor $EXTERNAL_MONITOR_2 detected. Setting it as primary."
-    xrandr --output "$EXTERNAL_MONITOR_2" --primary --auto --output "$INTERNAL_MONITOR" --off
-else
+for MONITOR in "${EXTERNAL_MONITORS[@]}"; do
+    STATUS=$(xrandr | grep "$MONITOR" | awk '{print $2}')
+    if [[ "$STATUS" == "connected" ]]; then
+        echo "External monitor $MONITOR detected. Setting it as primary and turning off internal monitor."
+        xrandr --output "$MONITOR" --primary --auto --output "$INTERNAL_MONITOR" --off
+        EXTERNAL_MONITOR_FOUND=true
+        break 
+    fi
+done
+
+if ! $EXTERNAL_MONITOR_FOUND; then
     echo "No external monitors detected. Setting $INTERNAL_MONITOR as primary."
-    xrandr --output "$INTERNAL_MONITOR" --primary --auto --output "$EXTERNAL_MONITOR_1" --off --output "$EXTERNAL_MONITOR_2" --off
+    TURNOFF_COMMAND="xrandr --output \"$INTERNAL_MONITOR\" --primary --auto"
+    for MONITOR in "${EXTERNAL_MONITORS[@]}"; do
+        TURNOFF_COMMAND+=" --output \"$MONITOR\" --off"
+    done
+    eval "$TURNOFF_COMMAND"
 fi
-
